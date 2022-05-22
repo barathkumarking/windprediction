369285a2d5fca74e6ab6b1b6614cc0269078a4ee
BARATHKUMAR G <62948487+barathkumarking@users.noreply.github.com>
2022-05-22 13:36:37 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,12 +1,4 @@
-"""
-Event:IBM Hack challenge 2020
-@team members: Barathkumar G  id:191cs138
-               Ajay S         id:191cs110
-               Dhanush s      id:191cs156
-Guide name: Ms Malathi T AP/CSE
-Mentor name : Mr Hemant Kumar Gahlot   AI Developer/IBM
-              Ms Gayatri   AI Developer/IBM
-"""
+
 import requests
 
 url = 'http://localhost:5000/predict_api'
